a9048bd9160f9407e0f54387206adfec40eaed8f
Esin Andrew <kubanesin@gmail.com>
2021-10-30 14:02:37 +0300

    Работа с свой-м submit рефакторинг

--- scripts/script.js
+++ scripts/script.js
@@ -9,17 +9,7 @@ let saveBtnProfile = document.querySelector('.popup__form');
 // находим popup
 let popup = document.querySelector('.popup');
 
-// находим имя и профессию в блоке profile
-let profileName = content.querySelector('.profile__name');
-let profileProfession = content.querySelector('.profile__profession');
 
-// находим Инпуты в popup
-let popupInputTypeName = document.querySelector('.popup__input_type_name');
-let popupInputTypeProfession = document.querySelector('.popup__input_type_profession');
-
-// заполняем Инпуты содержимым из профиля, имя и профессия
-popupInputTypeName.value = profileName.innerHTML;
-popupInputTypeProfession.value = profileProfession.innerHTML;
 
 // функции для открытия и закрытия popup
 function createProfilePopup() {
@@ -33,6 +23,18 @@ function closeProfilePopup() {
 // функция отправки формы (пока форму никуда не отправляем)
 function formSubmitHandler(evt) {
   evt.preventDefault();
+
+  // находим имя и профессию в блоке profile
+let profileName = content.querySelector('.profile__name');
+let profileProfession = content.querySelector('.profile__profession');
+
+// находим Инпуты в popup
+let popupInputTypeName = document.querySelector('.popup__input_type_name');
+let popupInputTypeProfession = document.querySelector('.popup__input_type_profession');
+
+// заполняем Инпуты содержимым из профиля, имя и профессия
+popupInputTypeName.value = profileName.innerHTML;
+popupInputTypeProfession.value = profileProfession.innerHTML;
 
   let nameInputValue = popupInputTypeName.value;
   let professionInputValue = popupInputTypeProfession.value;
