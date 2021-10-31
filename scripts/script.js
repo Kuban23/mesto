aa0843d7ed513f121ea7078ccdebbff89adc4d91
Esin Andrew <kubanesin@gmail.com>
2021-10-31 17:33:48 +0300

    JS_работа с сохраниением профиля и редактирование readme

--- scripts/script.js
+++ scripts/script.js
@@ -1,28 +1,28 @@
-// 1 находим кнонтент
+//  находим кнонтент
 let content = document.querySelector('.content');
 
-// 2 находим кнопоки
+//  находим кнопоки
 let editBtnProfile = content.querySelector('.profile__edit-button');
 let closeBtnPopup = document.querySelector('.popup__close');
 let saveBtnProfile = document.querySelector('.popup__form');
 
-// 3 находим popup
+//  находим popup
 let popup = document.querySelector('.popup');
 
-// 4 находим имя и профессию в блоке profile
+//  находим имя и профессию в блоке profile
 let profileName = content.querySelector('.profile__name');
 let profileProfession = content.querySelector('.profile__profession');
 
-// 5 находим Инпуты в popup
+//  находим Инпуты в popup
 let popupInputTypeName = document.querySelector('.popup__input_type_name');
 let popupInputTypeProfession = document.querySelector('.popup__input_type_profession');
 
-// 6 заполняем Инпуты содержимым из профиля, имя и профессия
+//  заполняем Инпуты содержимым из профиля, имя и профессия
 popupInputTypeName.value = profileName.innerHTML;
 popupInputTypeProfession.value = profileProfession.innerHTML;
 
 
-// 7 функции для открытия и закрытия popup
+//  функции для открытия и закрытия popup
 function createProfilePopup() {
   popup.classList.add('popup_opened');
 
@@ -33,21 +33,9 @@ function closeProfilePopup() {
 
 }
 
-// 8 функция отправки формы (пока форму никуда не отправляем)
+//  функция отправки формы (пока форму никуда не отправляем)
 function formSubmitHandler(evt) {
   evt.preventDefault();
-
-  //  4 находим имя и профессию в блоке profile
-  // let profileName = content.querySelector('.profile__name');
-  // let profileProfession = content.querySelector('.profile__profession');
-
-  // 5 находим Инпуты в popup
-  // let popupInputTypeName = document.querySelector('.popup__input_type_name');
-  // let popupInputTypeProfession = document.querySelector('.popup__input_type_profession');
-
-  // 6 заполняем Инпуты содержимым из профиля, имя и профессия
-  // popupInputTypeName.value = profileName.innerHTML;
-  // popupInputTypeProfession.value = profileProfession.innerHTML;
 
   let nameInputValue = popupInputTypeName.value;
   let professionInputValue = popupInputTypeProfession.value;
@@ -58,7 +46,7 @@ function formSubmitHandler(evt) {
   popup.classList.remove('popup_opened');
 }
 
-// 9 создали слушателя для кнопок
+//  создали слушателя для кнопок
 editBtnProfile.addEventListener('click', createProfilePopup);
 closeBtnPopup.addEventListener('click', closeProfilePopup);
 saveBtnProfile.addEventListener('submit', formSubmitHandler);
