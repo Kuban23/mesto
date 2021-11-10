83c1beca7590558bcf945412c57eab0603d0b08d
Esin Andrew <kubanesin@gmail.com>
2021-11-10 23:28:59 +0300

    JS сделал открытие и закрытие попапа для картинок

--- scripts/script.js
+++ scripts/script.js
@@ -1,34 +1,50 @@
 //  находим кнонтент
-let content = document.querySelector('.content');
+//const content = document.querySelector('.content');
 
 //  находим кнопоки
-let editBtnProfile = content.querySelector('.profile__edit-button');
-let closeBtnPopup = document.querySelector('.popup__close');
-let profileForm = document.querySelector('.popup__form');
+const editBtnProfile = document.querySelector('.profile__edit-button');
+const closeBtnPopupProfile = document.querySelector('.popup_type_profile .popup__close');
+const closeBtnPopupImage = document.querySelector('.popup_type_addImage .popup__close');
+const profileForm = document.querySelector('.popup__form');
+const addBtnProfile = document.querySelector('.profile__add-button');
 
 //  находим popup
-let popup = document.querySelector('.popup');
+const popupProfile = document.querySelector('.popup_type_profile');
+const popupAddImage = document.querySelector('.popup_type_addImage');
 
 //  находим Инпуты в popup
-let popupInputTypeName = document.querySelector('.popup__input_type_name');
-let popupInputTypeProfession = document.querySelector('.popup__input_type_profession');
+const popup = document.querySelector('.popup');
+const popupInputTypeName = document.querySelector('.popup__input_type_name');
+const popupInputTypeProfession = document.querySelector('.popup__input_type_profession');
 
 //  находим имя и профессию в блоке profile
-let profileName = content.querySelector('.profile__name');
-let profileProfession = content.querySelector('.profile__profession');
+let profileName = document.querySelector('.profile__name');
+let profileProfession = document.querySelector('.profile__profession');
 
-//  функции для открытия и закрытия popup
-function createProfilePopup() {
-  popup.classList.add('popup_opened');
+//  функции для открытия и закрытия popup profile
+function createProfilePopup(popupElement) {
+  popupElement.classList.add('popup_opened');
+
   //  заполняем Инпуты содержимым из профиля, имя и профессия
   popupInputTypeName.value = profileName.innerHTML;
   popupInputTypeProfession.value = profileProfession.innerHTML;
 }
 
-function closeProfilePopup() {
-  popup.classList.remove('popup_opened');
-
+function closeProfilePopup(popupElement) {
+  popupElement.classList.remove('popup_opened');
 }
+
+//  функции для открытия и закрытия popup addImage
+
+// function createAddImagePopup() {
+//   popupAddImage.classList.add('popup_opened');
+// }
+
+// function closeAddImagePopup() {
+//   popupAddImage.classList.remove('popup_opened');
+// }
+
+
 
 //  функция отправки формы (пока форму никуда не отправляем)
 function formSubmitHandler(evt) {
@@ -39,12 +55,33 @@ function formSubmitHandler(evt) {
   profileProfession.textContent = popupInputTypeProfession.value;
 
   // закрываем popup
-  closeProfilePopup();
+  closeProfilePopup(popupProfile);
 }
 
+
+
 //  создали слушателя для кнопок
-editBtnProfile.addEventListener('click', createProfilePopup);
-closeBtnPopup.addEventListener('click', closeProfilePopup);
+editBtnProfile.addEventListener('click', function () { // Открываем popup profile
+  createProfilePopup(popupProfile);
+});
+
+addBtnProfile.addEventListener('click', function () {  // Открываем popup для добавления картинок
+  createProfilePopup(popupAddImage);
+});
+
+
+closeBtnPopupProfile.addEventListener('click', function () { // Закрываем popup profile
+  closeProfilePopup(popup);
+});
+
+closeBtnPopupImage.addEventListener('click', function () { // Закрываем popup картинок
+  closeProfilePopup(popupAddImage);
+});
+
+
 profileForm.addEventListener('submit', formSubmitHandler);
 
+
+
+// closeBtnPopup.addEventListener('click', closeAddImagePopup);
 
