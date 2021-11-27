6754e8000bb68db6acd4e380a7ccd8a877278797
Esin Andrew <kubanesin@gmail.com>
2021-11-27 15:19:38 +0300

    Реализовал валидацию двух popup

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,41 +1,68 @@
-const formElement = document.querySelector('.popup__form');
-const formInput = formElement.querySelector('.popup__input'); // потом нужно взять коллекцию
+// Добавление обработчиков всем Инпутам
+const setEventListeners = (formElement) => {
+  // Находим все Инпуты и помещаем их в массив
+  const inputList = Array.from(formElement.querySelectorAll('.popup__input'));
 
-// Отменяем стандартное поведение, отправку формы
-formElement.addEventListener('submit', function (evt) {
-  evt.preventDefault();
-});
+  // Перебераем массив с коллекцией и добавим каждому Инпуту обработчик
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener('input', function () {
+      isValid(formElement, inputElement);
+    });
+  });
+};
 
 
-// Слушатель события Input
-formInput.addEventListener('input', function (evt) {
-  console.log(evt.target.validity.valid);
-});
+// Добавление обработчиков всем Формам
+const enableValidation = () => {
+  const formList = Array.from(document.querySelectorAll('.popup__form'));
+  formList.forEach((formElement) => {
+    formElement.addEventListener('submit', function (evt) {
+      evt.preventDefault();
+    });
+    // Для каждой формы вызываем функцию setEventListeners для добавления обработчиков
+    setEventListeners(formElement);
+  });
+};
+
+enableValidation();
+
 
 
 // Функция, которая добавляет класс с ошибкой
-const showInputError = (element) => {
-  element.classList.add('popup__input_type_error');
+const showInputError = (formElement, inputElement, errorMassage) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`); // находим элемент ошибки
+  inputElement.classList.add('popup__input_type_error');
+  // Показываем сообщение об ошибке
+  errorElement.classList.add('popup__input-error');
+  errorElement.textContent = errorMassage;
 };
 
 
 // Функция, которая удаляет класс с ошибкой
-const hideInputError = (element) => {
-  element.classList.remove('popup__input_type_error');
+const hideInputError = (formElement, inputElement) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`); // находим элемент ошибки
+  inputElement.classList.remove('popup__input_type_error');
+  // Убираем сообщение об ошибке
+  errorElement.classList.remove('popup__input-error');
+  errorElement.textContent = '';
 };
 
 
-// Функция, которая проверяет валидность Input
-const isValid = () => {
-  if (!formInput.validity.valid) {
-    showInputError(formInput);
+// Функция, которая проверяет валидность Input, принимает элементы формы и инпута ввиде аргумента
+const isValid = (formElement, inputElement) => {
+  if (!inputElement.validity.valid) {
+    // Функция showInputError принимает параметром форму в которой находится проверяемое поле и само это поле.
+    showInputError(formElement, inputElement, inputElement.validationMessage);
   }
   else {
-    hideInputError(formInput);
+    // Функция hideInputError принимает параметром форму в которой находится проверяемое поле и само это поле.
+    hideInputError(formElement, inputElement);
   }
 };
 
 
-// Вызываем функцию isValid при каждом нажатии клавиши в поле Input
-formInput.addEventListener('input', isValid);
+
+
+
+
 
