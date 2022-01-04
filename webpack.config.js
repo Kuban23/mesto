52147468978738c0a67b53fe5bfcc17261fb2740
Esin Andrew <kubanesin@gmail.com>
2022-01-04 19:36:27 +0300

    Настроил обработку CSS

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,9 @@ const path = require('path');  // подключаем path к конфигу в
 const HtmlWebpackPlugin = require('html-webpack-plugin'); // подключите плагин HTML
 const { CleanWebpackPlugin } = require('clean-webpack-plugin'); // подключили плагин
 
+// подключите к проекту mini-css-extract-plugin
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 module.exports = {
   entry: {
     main: './src/pages/index.js'
@@ -30,12 +33,22 @@ module.exports = {
         // исключает папку node_modules, файлы в ней обрабатывать не нужно
         exclude: '/node_modules/'
       },
-    // добавили правило для обработки файлов
-  {
-    // регулярное выражение, которое ищет все файлы с такими расширениями
-    test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
-    type: 'asset/resource'
-  },]
+      // добавили правило для обработки файлов
+      {
+        // регулярное выражение, которое ищет все файлы с такими расширениями
+        test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+        type: 'asset/resource'
+      },
+      {
+        // применять это правило только к CSS-файлам
+        test: /\.css$/,
+        // при обработке этих файлов нужно использовать
+        // MiniCssExtractPlugin.loader и css-loader
+        use: [MiniCssExtractPlugin.loader, {
+          loader: 'css-loader'
+        }]
+      }
+    ]
   },
   // добавьте массив
   plugins: [
@@ -43,6 +56,7 @@ module.exports = {
       template: './src/index.html' // путь к файлу index.html
     }),
     new CleanWebpackPlugin(), // использовали плагин
+    new MiniCssExtractPlugin() // подключение плагина для объединения файлов
   ]
 
 
