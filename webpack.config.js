a6d8a535455c9a859337241cb28e80a795c5df38
Esin Andrew <kubanesin@gmail.com>
2022-01-04 19:20:37 +0300

    Настроил обработку изображений и шрифтов

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,13 @@ module.exports = {
         use: 'babel-loader',
         // исключает папку node_modules, файлы в ней обрабатывать не нужно
         exclude: '/node_modules/'
-      }]
+      },
+    // добавили правило для обработки файлов
+  {
+    // регулярное выражение, которое ищет все файлы с такими расширениями
+    test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+    type: 'asset/resource'
+  },]
   },
   // добавьте массив
   plugins: [
